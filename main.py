1aa568d7854d621f70eb00db85bee40ee1ff5e65
cheddar-cheeze <cheddarcheeze@protonmail.com>
2018-08-23 18:47:19 -0500

    working filter

--- main.py
+++ main.py
@@ -57,10 +57,14 @@ def tail(bot, job, file):
                     dict_message.append(','.join(row).strip(','))
                 new_id = int(dict_message[2]) + 76561197960265728
                 cleaned_name = dict_message[4].replace('[', '').replace(']', '')
-                md = "(catbot-%c) \n [ %n ](https://steamcommunity.com/profiles/%i): %m"
-                new = md.replace('%n', cleaned_name).replace('%m', dict_message[6]).replace('%i', str(new_id)).replace('%c', dict_message[-1])
+                md = "[ %n ](https://steamcommunity.com/profiles/%i): %m"
+                new = md.replace('%n', cleaned_name).replace('%m', dict_message[6]).replace('%i', str(new_id))
                 print(new)
-                message_queue.append(new)
+                message_obj = {
+                    'ipc_id': dict_message[-1],
+                    'text': new
+                }
+                message_queue.append(message_obj)
         except Exception as e:
             print(e)
 
@@ -72,19 +76,37 @@ def relay(bot ,job):
 
 def message_send(bot, job):
     if message_queue[:]:
+        filter_list = []
+        for message in message_queue:
+            filter_list.append(message['text'])
+        a, seen, result = filter_list, set(), []
+        for idx, item in enumerate(a):
+            if item not in seen:
+                seen.add(item)
+            else:
+                result.append(idx)
+        for indx in result:
+            print('Removing duplicate at queue pos:' + str(indx))
+            message_queue.pop(indx)
+        del filter_list[:]
         print("Sending Messages!")
         try:
-            cleaned_queue = set(message_queue)
-            for message in cleaned_queue:
-                bot.send_message(chat_id='-1001203927071', text=message, parse_mode='MARKDOWN', disable_web_page_preview=True, disable_notification=True)
+            for message_obj in message_queue:
+                message = "*cat-bot " + str(message_obj['ipc_id']) + "* \n" + str(message_obj['text'])
+                try:
+                    bot.send_message(chat_id='-1001203927071', text=message, parse_mode='MARKDOWN', disable_web_page_preview=True, disable_notification=True)
+                except Exception as e:
+                    break
             del message_queue[:]
-            print("Messages Sent!")
         except Exception as e:
             print(e)
+        finally:
+            print("Messages Sent!")
 
 dispatcher.add_handler(CommandHandler("start", start))
 dispatcher.add_handler(CommandHandler("say", say, pass_args=True))
 dispatcher.add_handler(CommandHandler("sayall", sayall, pass_args=True))
 updater.start_polling()
 job.run_once(relay, 1)
-job.run_repeating(message_send, interval=5, first=0)
+job.run_repeating(message_send, interval=10, first=0)
+updater.idle()
